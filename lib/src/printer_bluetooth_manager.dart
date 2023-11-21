b14ced6ba46e3d2fb3f98394fd98294e6e3a169f
trienss <trienss@outlook.com.vn>
2023-11-21 21:51:15 +0700

    change esc post utils

--- lib/src/printer_bluetooth_manager.dart
+++ lib/src/printer_bluetooth_manager.dart
@@ -8,7 +8,6 @@
 
 import 'dart:async';
 import 'dart:io';
-import 'package:esc_pos_utils/esc_pos_utils.dart';
 import 'package:rxdart/rxdart.dart';
 import 'package:flutter_bluetooth_basic/flutter_bluetooth_basic.dart';
 import './enums.dart';
